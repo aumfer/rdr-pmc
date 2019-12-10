44975ae63c9c253ced37a2da8e937a43c663bb8c
Anthony-Umfer <Anthony.Umfer@coxautoinc.com>
2019-12-09 22:44:16 -0500

    Update variables.tf

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -6,6 +6,18 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
+module "tags" {
+  source    = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=0.4.0"
+  name      = "${var.repo_name}-${var.branch_name}"
+  namespace = local.namespace
+  stage     = local.stage
+  tags = {
+    repo = var.repo_name
+    env  = var.branch_name
+    sha  = var.source_rev
+  }
+}
+
 locals {
   namespace = "rdr"
 
