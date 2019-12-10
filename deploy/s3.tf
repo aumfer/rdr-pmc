cae0b39ced3734832516b1366b2a5e26176fe619
Anthony-Umfer <Anthony.Umfer@coxautoinc.com>
2019-12-09 22:21:04 -0500

    Update s3.tf

--- deploy/s3.tf
+++ deploy/s3.tf
@@ -1,7 +1,7 @@
 module "website" {
   source         = "git::https://github.com/cloudposse/terraform-aws-s3-website.git?ref=0.8.0"
-  namespace      = locals.namespace
-  stage          = locals.state
+  namespace      = local.namespace
+  stage          = local.stage
   name           = "${var.repo_name}-${var.branch_name}"
   hostname       = "${var.branch_name}.${var.repo_name}.alteredco.com"
   parent_zone_id = data.aws_route53_zone.primary.zone_id
